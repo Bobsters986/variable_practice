4fa9cbac70da1e4588a43c50b3113b6395d4916e
Bobby Luly <bobbyluly@gmail.com>
2022-11-01 16:19:15 -0600

    Reassign string variables

--- variables.rb
+++ variables.rb
@@ -10,6 +10,11 @@ first_pay = 10.50
 recent_pay = 28.50
 ideal_salary = 100,000.00
 
-excited = false
-loves_ruby = false
-good_student = false
+excited = true
+loves_ruby = true
+good_student = true
+
+name = "bobby boy"
+title = "awesome student"
+fav_hobby = "video games"
+
